ccc2644402773b0d535ed41bd0a6628f294df41f
Nezer_ag95 <Nezer_ag95@STHQ0124-12>
2023-02-27 20:09:28 +0400

    Add project files.

---
+++ Qacan Button/Form1.cs
@@ -0,0 +1,26 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qacan_Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            button1.Location = new Point(rand.Next(0, this.Size.Width - 100), rand.Next(0, this.Size.Height - 100));
+        }
+    }
+}
